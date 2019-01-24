c1dd6383b7c2231cd216fc9a61a7558430eeb2db
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-24 20:48:58 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,6 @@
 def prime?(integer)
-  (2..integer - 1).each {|x| return false if (integer % x) == 0}
+  if (2..integer - 1).each {|x| return false if (integer % x) == 0}
   true
+else
+  false
 end
